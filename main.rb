adc141071d26ef4f32c2653b53bd8428d4495333
Rebecca Chen <rebecca061996@gmail.com>
2021-03-01 16:44:57 +0000

    remove game tests and add code for running game

--- main.rb
+++ main.rb
@@ -2,11 +2,6 @@ require './player'
 require './game'
 require './prompt'
 
-player1 = Player.new
-player2 = Player.new
-
 math_game = Game.new
-game_prompts = Prompt.new
 
-game_prompts.math_question
-game_prompts.check_answer
+math_game.start_game
