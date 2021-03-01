e5c9e6e1fb6462028a1be38acd2ad79485d38b50
Rebecca Chen <rebecca061996@gmail.com>
2021-03-01 16:44:26 +0000

    update math question prompt to take in the current player to display correct question prompt

--- prompt.rb
+++ prompt.rb
@@ -1,17 +1,19 @@
 class Prompt
-  def math_question
+  def math_question(player)
     @numb1 = generate_number
     @numb2 = generate_number
     @answer = @numb1 + @numb2
-    puts "What does #{@numb1} plus #{@numb2} equal?"
+    puts "Player #{player}: What does #{@numb1} plus #{@numb2} equal?"
   end
 
   def check_answer
     player_answer = gets.chomp.to_i
     if player_answer == @answer
       puts 'YES! You are correct'
+      return true
     else
       puts 'Seriously? No!'
+      return false
     end
   end
 
