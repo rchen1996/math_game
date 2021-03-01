c4805eb192fabe3bc2da540e279a7bc6ce6fd0e4
Rebecca Chen <rebecca061996@gmail.com>
2021-03-01 16:12:57 +0000

    implement functionality for prompt class

---
+++ prompt.rb
@@ -0,0 +1,23 @@
+class Prompt
+  def math_question
+    @numb1 = generate_number
+    @numb2 = generate_number
+    @answer = @numb1 + @numb2
+    puts "What does #{numb1} plus #{numb2} equal?"
+  end
+
+  def check_answer(input)
+    player_answer = gets.chomp
+    if player_answer == @answer
+      puts 'YES! You are correct'
+    else
+      puts 'Seriously? No!'
+    end
+  end
+
+  private
+
+  def generate_number
+    rand(1..20)
+  end
+end
